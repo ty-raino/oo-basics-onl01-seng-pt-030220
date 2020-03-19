c97ceb51ea5e9a626f5c2e8ba0a19a72cb16ad99
Ty Raino <mstymetris@yahoo.com>
2020-03-19 21:35:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
   
+  def cobble
+    puts "The shoe has been repaired"
 end
